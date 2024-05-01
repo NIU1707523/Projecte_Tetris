d542e214b5d7c46811c59c4e00f2906b556c9f37
NIU1707523 <email@example.com>
2024-05-01 12:47:39 +0200

    Tauler_Commit(2)

--- Programa_Tetris/Archivos de encabezado/Tauler.h
+++ Programa_Tetris/Archivos de encabezado/Tauler.h
@@ -7,5 +7,25 @@ const int MAX_COL = 8;
 
 class Tauler
 {
+public:
+
+    Tauler(int files, int columnes);
+    void setfiles(const int files) { nFiles = files; }
+    void setcolumnes(const int columnes) { nColumnes = columnes; }
+    int getfiles() const { return nFiles; }
+    int getColumnes() const { return nColumnes; }
+    bool colocarFigura(const Figura& figura);
+    bool movimentValid(const Figura& figura, int novaFila, int novaColumna);
+    bool girValid(const Figura& figura);
+    void eliminarFila(int fila);
+
+
+private:
+
+    int nFiles, nColumnes;
+    int tauler[MAX_FILA][MAX_COL];
+    bool filaCompleta(int fila) const;
+
 };
+
 #endif
--- Programa_Tetris/Archivos de origen/Tauler.cpp
+++ Programa_Tetris/Archivos de origen/Tauler.cpp
@@ -1,1 +1,26 @@
 #include "Tauler.h"
+
+Tauler::Tauler(int files, int columnes)
+{
+    nFiles = files;
+    nColumnes = columnes;
+
+    for (int i = 0; i < nFiles; i++)
+    {
+        for (int j = 0; i < nColumnes; i++)
+        {
+            tauler[i][j] = -1;
+        }
+    }
+}
+
+
+
+bool Tauler::eliminarFila(int fila)
+{
+    for (int i = 0; i < nFiles; i++)
+    {
+        tauler[fila][i] = -1;
+
+    }
+}
